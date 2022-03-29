3f6e9ff15bda6bfaefd4c133e75263f7d9885a20
Nathan Krulic <krulic1325@gmail.com>
2022-03-29 15:34:46 +0200

    TD5

---
+++ TD6/RESTWebServices/IMathsOperations.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace RESTWebServices
+{
+    // REMARQUE : vous pouvez utiliser la commande Renommer du menu Refactoriser pour changer le nom d'interface "IService1" à la fois dans le code et le fichier de configuration.
+    [ServiceContract]
+    public interface IMathsOperations
+    {
+        [OperationContract]
+        [WebInvoke(UriTemplate = "Add?x={a}&y={b}", ResponseFormat = WebMessageFormat.Json, BodyStyle = WebMessageBodyStyle.Wrapped, Method = "GET")]
+        int Add(int a, int b);
+
+        [OperationContract]
+        [WebInvoke(UriTemplate = "Multiply?x={a}&y={b}", ResponseFormat = WebMessageFormat.Json, BodyStyle = WebMessageBodyStyle.Wrapped, Method = "GET")]
+        int Multiply(int a, int b);
+
+        [OperationContract]
+        [WebInvoke(UriTemplate = "Substract?x={a}&y={b}", ResponseFormat = WebMessageFormat.Json, BodyStyle = WebMessageBodyStyle.Wrapped, Method = "GET")]
+        int Substract(int a, int b);
+
+        [OperationContract]
+        CompositeType GetDataUsingDataContract(CompositeType composite);
+
+        // TODO: ajoutez vos opérations de service ici
+    }
+
+    // Utilisez un contrat de données comme indiqué dans l'exemple ci-après pour ajouter les types composites aux opérations de service.
+    // Vous pouvez ajouter des fichiers XSD au projet. Une fois le projet généré, vous pouvez utiliser directement les types de données qui y sont définis, avec l'espace de noms "SOAPWebService.ContractType".
+    [DataContract]
+    public class CompositeType
+    {
+        bool boolValue = true;
+        string stringValue = "Hello ";
+
+        [DataMember]
+        public bool BoolValue
+        {
+            get { return boolValue; }
+            set { boolValue = value; }
+        }
+
+        [DataMember]
+        public string StringValue
+        {
+            get { return stringValue; }
+            set { stringValue = value; }
+        }
+    }
+}
---
+++ TD6/SOAPWebClient/Program.cs
@@ -0,0 +1,21 @@
+﻿using SOAPWebClient.ServiceReference1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOAPWebClient
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MathsOperationsClient calculator = new ServiceReference1.MathsOperationsClient();
+            Console.WriteLine("3 + 3 = "+calculator.Add(3, 3));
+            Console.WriteLine("4 - 2 = "+calculator.Substract(4, 2));
+            Console.WriteLine("4 * 2 = "+calculator.Multiply(4, 2));
+            Console.ReadLine();
+        }
+    }
+}
